95db28a313e7670fb16124d9da66229cc22ceb4a
Abdurakxmon <77540298+Abdurakxmon@users.noreply.github.com>
2025-02-14 18:52:42 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,9 +1,9 @@
 import time 
 
 def decorator_1(f):
-    def solve(**kwargs):
+    def solve(*args, **kwargs):
         start = time.time()
-        ans = f(**kwargs)
+        ans = f(*args, **kwargs)
         print(f"func call executed in {round(time.time()-start, 6)} sec")
         return ans
     return solve
