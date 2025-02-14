5c8fd2c630c51775e231108f5d41d19ba10505cb
Abdurakxmon <77540298+Abdurakxmon@users.noreply.github.com>
2025-02-14 18:34:52 +0500

    2nd task main.py

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ from .task2 import typeBasedTransformer
 from .task3 import decorator_1
 
 kwargsAcceptFun(num=1e6, str="Testing", arr=[1, 2, 3])
+
+
+
+data = {
+    "num": 1e6,
+    "dec": 0.1+0.3,
+    "str": "Testing",
+    "flag": True,
+    "arr": [1, 2, 3],
+    "tpl": ("a", "b", "c"),
+    "dict": {"a": 1, "b": 2},
+    "set": {1, 2, 3},
+}
+
+print(typeBasedTransformer(**data))
+
+
+
